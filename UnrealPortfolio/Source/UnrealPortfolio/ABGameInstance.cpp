b3029522655f2fc82386ddbdbbe9a33a90e7e6a8
Si Yun (Steven Kim) <dev@donga.ac.kr>
2020-06-16 13:12:19 +0900

    #academy
    update

---
+++ UnrealPortfolio/Source/UnrealPortfolio/ABGameInstance.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABGameInstance.h"
+
+UABGameInstance::UABGameInstance()
+{
+	FString CharacterDataPath = TEXT("/Game/Book/GameData/ABCharacterData.ABCharacterData");
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT_ABCHARACTER(*CharacterDataPath);
+	ABCHECK(DT_ABCHARACTER.Succeeded());
+	ABCharacterTable = DT_ABCHARACTER.Object;
+	ABCHECK(ABCharacterTable->RowMap.Num() > 0);
+
+}
+
+void UABGameInstance::Init()
+{
+	Super::Init();
+	ABLOG_S(Warning);
+
+	ABLOG(Warning, TEXT("DropExp of Level 20 ABCharacter : %d"), GetABCharacterData(20)->DropExp);
+
+	FABCharacterData* UABGameInstance::GetABCharacterData(int32 level)
+	{
+		return ABCharacterTable->FindRow<FABCharacterData>(*FString::FromInt(Level), TEXT(""));
+	}
+
+}
